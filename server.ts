077b1482cef2a28d039e454787dbcb3458a65bd7
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-05-20 13:11:13 +0100

    paths for likes and comments

--- server.ts
+++ server.ts
@@ -31,8 +31,8 @@ client.connect();
 app.get("/All", async (req, res) => {
   res.set('access-control-allow-origin', '*')
   try {
-  const dbres = await client.query('select * from recommendations order by date desc');
-  res.json(dbres.rows);
+    const dbres = await client.query('select * from recommendations order by date desc');
+    res.json(dbres.rows);
   } catch (error) {
     res.status(404)
     console.error(error)
@@ -57,8 +57,8 @@ app.get("/All", async (req, res) => {
 app.get("/recent", async (req, res) => {
   res.set('access-control-allow-origin', '*')
   try {
-  const dbres = await client.query('select * from recommendations order by date desc limit 10');
-  res.json(dbres.rows);
+    const dbres = await client.query('select * from recommendations order by date desc limit 10');
+    res.json(dbres.rows);
   } catch (error) {
     res.status(404)
     console.error(error)
@@ -69,9 +69,9 @@ app.get("/recent", async (req, res) => {
 app.post("/", async (req, res) => {
   res.set('access-control-allow-origin', '*')
   try {
-  const recom:iPostRecommendation = req.body
-  const dbres = await client.query('insert into recommendations(user_id,author,url,title,description,tags,content_type,rating,reason,build_week) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) returning *',[recom.user_id, recom.author,recom.url,recom.title,recom.description,recom.tags,recom.content_type,recom.rating,recom.reason,recom.build_week]);
-  res.json(dbres.rows);
+    const recom: iPostRecommendation = req.body
+    const dbres = await client.query('insert into recommendations(user_id,author,url,title,description,tags,content_type,rating,reason,build_week) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) returning *', [recom.user_id, recom.author, recom.url, recom.title, recom.description, recom.tags, recom.content_type, recom.rating, recom.reason, recom.build_week]);
+    res.json(dbres.rows);
   } catch (error) {
     res.json(error.detail)
     console.error(error.detail)
@@ -80,35 +80,35 @@ app.post("/", async (req, res) => {
 
 //
 app.get("/:selector/:searchItem", async (req, res) => {
- // res.set('access-control-allow-origin', '*')
+  // res.set('access-control-allow-origin', '*')
   try {
-  const selector = req.params.selector
-  const searchItem = req.params.searchItem
-  let queryString:string;
-  if (selector === "tags") {
-  queryString = `select * from recommendations where $1=ANY(tags)`
-  const dbres = await client.query(queryString, [searchItem])
-  res.json(dbres.rows)
-  
-  }
-  
-  else if (selector === 'All'){
-    queryString = "select * from recommendations where lower(author) like lower($1) or lower(title) like lower($2) order by date desc"
-    const wildcardSearch = "%"+searchItem+"%"
-    const dbres = await client.query(queryString, [wildcardSearch, wildcardSearch])
-    
-    res.json(dbres.rows)
+    const selector = req.params.selector
+    const searchItem = req.params.searchItem
+    let queryString: string;
+    if (selector === "tags") {
+      queryString = `select * from recommendations where $1=ANY(tags)`
+      const dbres = await client.query(queryString, [searchItem])
+      res.json(dbres.rows)
 
-  }
-  
-  else{
-    queryString = `select * from recommendations where lower(${selector}) like lower($1) order by date desc`;
-    const wildcardSearch = "%"+searchItem+"%"
-    const dbres = await client.query(queryString, [wildcardSearch])
-    res.json(dbres.rows)
-  }
+    }
 
-  }catch (error) {
+    else if (selector === 'All') {
+      queryString = "select * from recommendations where lower(author) like lower($1) or lower(title) like lower($2) order by date desc"
+      const wildcardSearch = "%" + searchItem + "%"
+      const dbres = await client.query(queryString, [wildcardSearch, wildcardSearch])
+
+      res.json(dbres.rows)
+
+    }
+
+    else {
+      queryString = `select * from recommendations where lower(${selector}) like lower($1) order by date desc`;
+      const wildcardSearch = "%" + searchItem + "%"
+      const dbres = await client.query(queryString, [wildcardSearch])
+      res.json(dbres.rows)
+    }
+
+  } catch (error) {
     res.status(404).send("can't get from database")
     console.error(error)
   }
@@ -117,39 +117,39 @@ app.get("/:selector/:searchItem", async (req, res) => {
 // get list of users
 app.get("/users", async (req, res) => {
   res.set('access-control-allow-origin', '*')
-try{
-  const dbres = await client.query("select * from users order by name")
-  res.json(dbres.rows)
-}
-catch(error){
-  res.status(404).send("can't get from database")
-  console.error(error)
-}
+  try {
+    const dbres = await client.query("select * from users order by name")
+    res.json(dbres.rows)
+  }
+  catch (error) {
+    res.status(404).send("can't get from database")
+    console.error(error)
+  }
 })
 
 // get user's saved recommendations
 app.get("/users/saved/:id", async (req, res) => {
   res.set('access-control-allow-origin', '*')
-try{
-  const id = parseInt(req.params.id)
-  const dbres = await client.query("WITH saved_array AS(select saved_recommendations from users where user_id = $1)select * from recommendations where id=any(SELECT unnest(saved_recommendations) FROM saved_array)", [id])
-  res.json(dbres.rows)
-  
+  try {
+    const id = parseInt(req.params.id)
+    const dbres = await client.query("WITH saved_array AS(select saved_recommendations from users where user_id = $1)select * from recommendations where id=any(SELECT unnest(saved_recommendations) FROM saved_array)", [id])
+    res.json(dbres.rows)
 
-}
-catch(error){
-  res.status(404).send("can't get from database")
-  console.error(error)
-}
+
+  }
+  catch (error) {
+    res.status(404).send("can't get from database")
+    console.error(error)
+  }
 })
 //add a saved recommendation 
 app.patch("/users/addsaved/:id/:savedrec", async (req, res) => {
   res.set('access-control-allow-origin', '*')
   try {
-  const id = parseInt(req.params.id)
-  const savedrec = parseInt(req.params.savedrec)
-  const dbres = await client.query('update users set saved_recommendations = array_append(saved_recommendations, $1) where user_id = $2 returning *', [savedrec, id]);
-  res.json(dbres.rows)
+    const id = parseInt(req.params.id)
+    const savedrec = parseInt(req.params.savedrec)
+    const dbres = await client.query('update users set saved_recommendations = array_append(saved_recommendations, $1) where user_id = $2 returning *', [savedrec, id]);
+    res.json(dbres.rows)
   } catch (error) {
     res.status(404).send("can't update database")
     console.error(error)
@@ -161,15 +161,84 @@ app.patch("/users/addsaved/:id/:savedrec", async (req, res) => {
 app.patch("/users/removesaved/:id/:savedrec", async (req, res) => {
   res.set('access-control-allow-origin', '*')
   try {
-  const id = parseInt(req.params.id)
-  const savedrec = parseInt(req.params.savedrec)
-  const dbres = await client.query('update users set saved_recommendations = array_remove(saved_recommendations, $1) where user_id = $2 returning *', [savedrec, id]);
-  res.json(dbres.rows)
+    const id = parseInt(req.params.id)
+    const savedrec = parseInt(req.params.savedrec)
+    const dbres = await client.query('update users set saved_recommendations = array_remove(saved_recommendations, $1) where user_id = $2 returning *', [savedrec, id]);
+    res.json(dbres.rows)
   } catch (error) {
     res.status(404).send("can't update database")
     console.error(error)
   }
 });
+
+// get comments
+app.get("/comments/:userid/:postid", async (req, res) => {
+  res.set('access-control-allow-origin', '*')
+  try {
+    const userid = parseInt(req.params.userid)
+    const postid = parseInt(req.params.postid)
+    const query = "select * from comments where user_id=$1 and post_id=$2"
+    const dbres = await client.query(query, [userid, postid])
+    res.json(dbres.rows)
+  }
+  catch (error) {
+    res.status(404).send("can't get from database")
+    console.error(error)
+  }
+})
+
+// post comment 
+app.post("/comments", async (req, res) => {
+  res.set('access-control-allow-origin', '*')
+  try {
+    const { userid, postid, comment } = req.body
+    const query = "insert into comments (user_id, post_id, comment) values ($1, $2, $3) returning *"
+    const dbres = await client.query(query, [userid, postid, comment])
+    res.json(dbres.rows);
+  } catch (error) {
+    res.status(400).send("can't post to database")
+    console.error(error)
+  }
+});
+
+// delete comment
+app.delete("/comments/:commentid", async (req, res) => {
+  res.set('access-control-allow-origin', '*')
+  try {
+    const commentid = req.params.commentid
+    const query = "delete from comments * where comment_id =$1 returning *"
+    const dbres = await client.query(query, [commentid])
+    res.json(dbres.rows)
+
+  }
+  catch (error) {
+    res.status(400).send("can't delete from database")
+    console.error(error)
+  }
+})
+
+// get likes 
+app.get("/likes", async (req, res) => {
+  res.set('access-control-allow-origin', '*')
+  try {
+    const queryLikes = "select post_id, sum(likes) as total_likes from likes where likes = 1 group by post_id"
+    const queryDislikes = "select post_id, sum(likes) as total_dislikes from likes where likes = -1 group by post_id"
+
+    const dbresLikes = await client.query(queryLikes)
+    const dbresDislikes = await client.query(queryDislikes)
+
+    const result = { likes: dbresLikes.rows, dislikes: dbresDislikes.rows }
+    res.json(result)
+
+  }
+  catch (error) {
+    res.status(404).send("can't get from database")
+    console.error(error)
+  }
+})
+
+// post likes 
+// insert into likes(user_id, post_id, likes) values (1, 7, 1)
 
 
 //Start the server on the given port
