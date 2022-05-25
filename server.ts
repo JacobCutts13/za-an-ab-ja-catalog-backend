fd41b10767a0987977b45d06a7fa0fa2217bba7b
Zahra <bghzahra@outlook.com>
2022-05-25 10:30:34 +0100

    added comment id to get request

--- server.ts
+++ server.ts
@@ -196,7 +196,7 @@ app.get("/comments/:postid", async (req, res) => {
   res.set('access-control-allow-origin', '*')
   try {
     const postid = parseInt(req.params.postid)
-    const query = "select users.name, comments.comment from comments join users on users.user_id = comments.user_id where comments.post_id=$1"
+    const query = "select users.name, comments.comment, comments.comment_id from comments join users on users.user_id = comments.user_id where comments.post_id=$1"
     const dbres = await client.query(query, [postid])
     res.json(dbres.rows)
   }
